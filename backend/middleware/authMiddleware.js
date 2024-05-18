765cf5917406e09ba99e0ff83466431db2edf45f
Aravind Reddy Patlolla <Aravindreddy23572@gmail.com>
2024-05-18 19:51:22 +0530

    Added authMiddleWare

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+// backend/middleware/authMiddleware.js
+const jwt = require('jsonwebtoken');
+
+exports.authenticateToken = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ error: 'Access denied, no token provided' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid token' });
+  }
+};
